911c2e1f5bb9c97c88e87ed9c246ed78be1b0ce1
Jatin Kumar Bollareddy <jatinkumarbollareddy98@gmail.com>
2018-09-25 16:45:48 +0530

    Update top.v

--- top.v
+++ top.v
@@ -10,29 +10,69 @@ output reg Output;
 
 reg [7:0] cAdd;
 reg q;
-reg na;
+reg[8:0] na;
 reg c;          //class flag
 reg [5:0] inputAddr;
 reg i_write;
 
 
-sram # (parameter ADDR_WIDTH = 6, DATA_WIDTH = 56, DEPTH = 64) sram_inst1 (
+    sram # (parameter ADDR_WIDTH = 6, DATA_WIDTH = 48, DEPTH = 64) sram_inst1 (
     .i_clk(clk),
-    .i_addr(inputAddr),
-    .i_write(i_write),
+    .i_addr(inputAddr1),
+    .i_write(i_write1),
     .i_data(),
     .o_data()
 );
 
-sram # (parameter ADDR_WIDTH = 6, DATA_WIDTH = 12, DEPTH = 64) sram_inst2 (
+    sram # (parameter ADDR_WIDTH = 6, DATA_WIDTH = 18, DEPTH = 64) sram_inst2 (
     .i_clk(clk),
-    .i_addr(inputAddr),
-    .i_write(i_write),
-    .i_data(),
-    .o_data()
+    .i_addr(inputAddr2),
+    .i_write(i_write2),
+    .i_da(),
+    .o_da()
 );
 
-// BRAM_TDP_MACRO_inst coef(
+
+assign ENA= 1b'1;
+assign na=0;
+always@(posedge clk)
+
+begin
+  
+if (~c)
+begin  
+    c1=o_data[47:40];c2=o_data[39:32];c3=o_data[31:24];c4=o_data[23:16];c5=o_data[15:8];c6=o_data[7:0];
+  
+t = ((A1*c1)+(A2*c2)+(A3*c3)+(A4*c4));
+
+    i_addr=na[7:0];
+  
+  if  (t < c6)
+    begin
+        assign na=o_data[17:9];  //child address including the chld or class bit, so total 11+1=12 bits
+    end
+  else
+    begin
+        assign na=DOA_c[8:0];
+    end
+
+
+    if na[17]==1
+    begin
+      assign c=1;
+      Output=na;
+     end
+end
+
+
+
+endmodule // top
+
+    
+    
+    
+    
+    // BRAM_TDP_MACRO_inst coef(
 // .DOA(DOA_f),//Outputport-Adata,widthdefinedbyREAD_WIDTH_Aparameter
 // .DOB(DOB),//Outputport-Bdata,widthdefinedbyREAD_WIDTH_Bparameter
 // .ADDRA(ADDRA),//Inputport-Aaddress,widthdefinedbyPortAdepth
@@ -104,37 +144,3 @@ sram # (parameter ADDR_WIDTH = 6, DATA_WIDTH = 12, DEPTH = 64) sram_inst2 (
 // .RST(RST)
 // //1-bitactivehighsynchronousreset
 // );
-assign ENA= 1b'1;
-assign na=0;
-always@(posedge clk)
-
-begin
-  
-if (~c)
-begin  
-c1=o_data[35:30];c2=o_data[29:24];c3=o_data[23:18];c4=o_data[17:12];c5=o_data[11:6];c6=o_data[5:0];
-  
-t = ((A1*c1)+(A2*c2)+(A3*c3)+(A4*c4));
-
-  i_addr=na;
-  
-  if  (t < c6)
-    begin
-          assign na=o_data[32:21];  //child address including the chld or class bit, so total 11+1=12 bits
-    end
-  else
-    begin
-        assign na=DOA_c[11:0];
-    end
-
-
-    if na[12]==1
-    begin
-      assign c=1;
-      Output=na;
-     end
-end
-
-
-
-endmodule // top
