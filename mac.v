42fa004703f2e31cabc86c2e42a66dd2eb675f6a
Yayi98 <superchintu98@gmail.com>
2018-09-25 17:45:05 +0530

    Add files via upload

--- mac.v
+++ mac.v
@@ -1,20 +1,20 @@
-module top (A1,A2,A3,A4, Output,clk);
+module top (A1,A2,A3,A4,A5,c1,c2,c3,c4,c5,c6,Output,clk);
 
-input[7:0] A1;
-input[7:0] A2;
-input[7:0] A3;
-input[7:0] A4;
-//input A5[7:0];
-input clk;
+input wire [7:0] A1;
+input wire [7:0] A2;
+input wire [7:0] A3;
+input wire [7:0] A4;
+input wire [7:0] A5;
+input wire [7:0] c1,c2,c3,c4,c5,c6;
+input wire clk;
 
-output Output;
+output reg Output;
 
 
 
 
-//reg [7:0] a1,a2,a3,a4;  // atributes stored in registers 
-reg[15:0] t,c5;
-reg [7:0] c1,c2,c3,c4;
+//reg [7:0] a1,a2,a3,a4;  // atributes stored in registers
+reg[15:0] t,c6;
 reg Output;
 
 
@@ -29,7 +29,7 @@ begin
 	t = ((A1*c1)+(A2*c2)+(A3*c3)+(A4*c4));
 	if (t < c5)
 		Output=1;
-	else 
+	else
 		Output=0;
 end
-endmodule 
+endmodule
