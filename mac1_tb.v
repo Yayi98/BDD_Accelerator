7a3dc98bda8c831724da8fa773d61068cfcf7f52
Jatin Kumar Bollareddy <jatinkumarbollareddy98@gmail.com>
2018-10-09 19:26:39 +0530

    Add files via upload
    Tb for mac; serial input, serial output.

---
+++ mac1_tb.v
@@ -0,0 +1,36 @@
+module mac1_tb();
+
+reg clk, rst;
+reg [7:0] a,b;
+reg [15:0] acc;
+
+macunit DUT (.clk(clk), .rst(rst), .a(a), .b(b), .acc(acc));
+
+
+
+always @(*) begin
+    #10 clk = ~clk;
+
+initial begin
+clk=1;
+rst=0;
+a=0; b=0;
+rst=1;
+#20
+rst=0;
+
+a=5; b=1; #20
+a=5; b=2; #20
+a=5; b=3; #20
+
+rst =1;
+#20
+rst=0;
+
+a=4; b=1; #20
+a=4; b=2; #20
+a=4; b=3; #20
+
+end
+
+endmodule
