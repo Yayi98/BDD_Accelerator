9670323d91a8dfd71fbc577a502f809513041418
ymc98 <superchintu98@gmail.com>
2018-10-10 18:56:44 +0530

    Working Mac

--- mac1.v
+++ mac1.v
@@ -1,29 +1,22 @@
- module macunit(a, b, clk,rst, acc);
-    input [7:0] a;
-    input [7:0] b;
-    input clk,rst;
-    output [15:0] acc;
-    
-           reg [15:0]acc;
-           reg [15:0]sum;
-           reg [15:0]prod;    
-           reg [15:0]regg;
-           
-always @(a,b,clk,rst) begin
+module macunit(a, b, clk,rst, acc);
+  input wire [7:0] a;
+  input wire [7:0] b;
+  input wire clk,rst;
+  output wire [15:0] acc;
 
-if(rst==1'b1)
-begin
-regg=0;
-end
-else
-begin
-regg=sum;
-end
+   reg [15:0]sum;
+   reg [15:0]prod;
+   reg [15:0]regg;
 
-
-prod=a*b;
-sum=prod+regg;
-
-acc=regg;
-end
+   always @(clk,rst) begin
+       if(rst==1'b1) begin
+           regg=0;
+       end
+       else begin
+           regg=sum;
+       end
+       prod=a*b;
+       sum=prod+regg;
+   end
+   assign acc=sum;
 endmodule
