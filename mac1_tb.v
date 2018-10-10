6f181b59fd33a6c1d6f3e5b3dfff93240282e388
ymc98 <superchintu98@gmail.com>
2018-10-11 01:10:51 +0530

    TB and waveform updated

--- mac1_tb.v
+++ mac1_tb.v
@@ -1,36 +1,32 @@
 module mac1_tb();
 
-reg clk, rst;
-reg [7:0] a,b;
-reg [15:0] acc;
+    reg clk, rst;
+    reg [7:0] a,b;
+    wire [15:0] acc;
 
-macunit DUT (.clk(clk), .rst(rst), .a(a), .b(b), .acc(acc));
+    macunit DUT (.clk(clk), .rst(rst), .a(a), .b(b), .acc(acc));
 
+    always @(*) begin
+        #20 clk = ~clk;
+    end
 
+    initial begin
+        clk=1;
+        rst=0;
+        a=0; b=0;
+        rst=1;
+        #20
+        rst=0;
 
-always @(*) begin
-    #10 clk = ~clk;
-
-initial begin
-clk=1;
-rst=0;
-a=0; b=0;
-rst=1;
-#20
-rst=0;
-
-a=5; b=1; #20
-a=5; b=2; #20
-a=5; b=3; #20
-#100
-rst =1;
-#20
-rst=0;
-
-a=4; b=1; #20
-a=4; b=2; #20
-a=4; b=3; #20
-#100
-end
-
+        a=5; b=1; #20
+        a=5; b=2; #20
+        a=5; b=3; #10
+        rst=1;
+        #20
+        rst=0;
+        #20
+        a=4; b=1; #20
+        a=4; b=2; #20
+        a=4; b=3;
+    end
 endmodule
