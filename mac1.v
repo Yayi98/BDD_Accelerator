7f99e9d1ba6edb0ba5694e7adc265fcb3093bfe0
ymc98 <superchintu98@gmail.com>
2018-10-10 18:58:59 +0530

    mac1.v indentation fix

--- mac1.v
+++ mac1.v
@@ -1,22 +1,22 @@
 module macunit(a, b, clk,rst, acc);
-  input wire [7:0] a;
-  input wire [7:0] b;
-  input wire clk,rst;
-  output wire [15:0] acc;
+    input wire [7:0] a;
+    input wire [7:0] b;
+    input wire clk,rst;
+    output wire [15:0] acc;
 
-   reg [15:0]sum;
-   reg [15:0]prod;
-   reg [15:0]regg;
+    reg [15:0]sum;
+    reg [15:0]prod;
+    reg [15:0]regg;
 
-   always @(clk,rst) begin
-       if(rst==1'b1) begin
-           regg=0;
-       end
-       else begin
-           regg=sum;
-       end
-       prod=a*b;
-       sum=prod+regg;
-   end
-   assign acc=sum;
+    always @(clk,rst) begin
+        if(rst==1'b1) begin
+            regg=0;
+        end
+        else begin
+            regg=sum;
+        end
+        prod=a*b;
+        sum=prod+regg;
+    end
+    assign acc=sum;
 endmodule
