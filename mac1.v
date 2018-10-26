caa051d631fe717bc2e01604a6e88ecd0fdbfb5c
mahesh15230 <36959431+mahesh15230@users.noreply.github.com>
2018-10-26 18:51:51 +0530

    Update mac1.v

--- mac1.v
+++ mac1.v
@@ -1,24 +1,20 @@
-
-module macunit (a, b, clk,rst, acc);
+module mac1 (a, b, clk,rst, acc);
     input wire [9:0] a;
     input wire [9:0] b;
     input wire clk,rst;
     output reg [19:0] acc;
-
-    reg [19:0]sum;
-    reg [19:0]prod;
-    reg [19:0]regg;
+    reg [19:0] prod,regg,sum;
 
     always @(clk,rst) begin
         if(rst==1'b1) begin
-            regg <= 0;
-            sum <= 0;
+            regg = 0;
+            sum = 0;
         end
         else begin
-            regg <= sum;
+            regg = sum;
         end
         prod = a*b;
         sum = prod+regg;
+	    acc = sum;
     end
-    acc<=sum;
 endmodule
