cdc5d847014b6662fc04cc01889cbc67da48fb1a
ymc98 <superchintu98@gmail.com>
2018-10-29 15:55:17 +0530

    Working mac

--- mac1.v
+++ mac1.v
@@ -4,19 +4,21 @@ module mac1 #(ATTR_WIDTH = 24, RAM1_DATA_WIDTH = 24) (inputattr, inputcoeff, clk
     input wire clk;
     output reg [19:0] acc;
 
-    reg [ATTR_WIDTH-1:0] inputattrreg;
-    reg [RAM1_DATA_WIDTH-1:0] inputcoeffreg;
+  //  reg [ATTR_WIDTH-1:0] inputattrreg;
+  //  reg [RAM1_DATA_WIDTH-1:0] inputcoeffreg;
     reg [19:0] prod,sum;
     //reg [19:0] regg;
     reg [9:0] a, b;
     reg [2:0] counter = -3'b001;
     reg rst = 1'b0;
+//    wire [19:0] accwire;
 
 
     always @(posedge clk) begin
 
-        inputattrreg = inputattr;
-        inputcoeffreg = inputcoeff;
+ //       inputattrreg = inputattr;
+ //       inputcoeffreg = inputcoeff;
+ //       acc = accwire;
 
         if(counter>3'b010) begin
             counter = 3'b000;
@@ -29,32 +31,33 @@ module mac1 #(ATTR_WIDTH = 24, RAM1_DATA_WIDTH = 24) (inputattr, inputcoeff, clk
 
         case(counter)
             3'b000 : begin
-                        b = {2'b00,inputcoeffreg[RAM1_DATA_WIDTH-1:RAM1_DATA_WIDTH-8]};
-                        a = {2'b00,inputattrreg[ATTR_WIDTH-1:ATTR_WIDTH-8]};
+                        b = {2'b00,inputcoeff[RAM1_DATA_WIDTH-1:RAM1_DATA_WIDTH-8]};
+                        a = {2'b00,inputattr[ATTR_WIDTH-1:ATTR_WIDTH-8]};
                     end
 
             3'b001 : begin
-                        b = {2'b00,inputcoeffreg[RAM1_DATA_WIDTH-9:RAM1_DATA_WIDTH-16]};
-                        a = {2'b00,inputattrreg[ATTR_WIDTH-9:ATTR_WIDTH-16]};
+                        b = {2'b00,inputcoeff[RAM1_DATA_WIDTH-9:RAM1_DATA_WIDTH-16]};
+                        a = {2'b00,inputattr[ATTR_WIDTH-9:ATTR_WIDTH-16]};
                     end
             3'b010 : begin
-                        b = {2'b00,inputcoeffreg[RAM1_DATA_WIDTH-17:RAM1_DATA_WIDTH-24]};
-                        a = {2'b00,inputattrreg[ATTR_WIDTH-17:ATTR_WIDTH-24]};
+                        b = {2'b00,inputcoeff[RAM1_DATA_WIDTH-17:RAM1_DATA_WIDTH-24]};
+                        a = {2'b00,inputattr[ATTR_WIDTH-17:ATTR_WIDTH-24]};
                         acc = sum;
                     end
             3'b011 : begin
                         //b <= inputcoeffreg[RAM1_DATA_WIDTH-25:RAM1_DATA_WIDTH-34];
                         rst = 1'b1;
+                        acc = sum;
 					end
 
             default : begin
-                        inputattrreg = 8'bx;
+                    //    inputattrreg = 8'bx;
                         b = 8'bx;
                       end
         endcase
     end
 
-    always @(clk,rst) begin
+    always @(posedge clk) begin
         if(rst==1'b1) begin
             sum = 0;
         end else begin
@@ -63,4 +66,5 @@ module mac1 #(ATTR_WIDTH = 24, RAM1_DATA_WIDTH = 24) (inputattr, inputcoeff, clk
             sum = prod+sum;
         end
     end
+ //   assign accwire = sum;
 endmodule
