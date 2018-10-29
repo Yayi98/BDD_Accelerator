fedb523dfb59d13f04cab06f54a681cc4f40966e
ymc98 <superchintu98@gmail.com>
2018-10-29 19:21:14 +0530

    New clkdiv added

--- clkdiv2.v
+++ clkdiv2.v
@@ -1,13 +1,12 @@
 module  clkdiv2 (clk,clk1,clk2);
- // D flip flop one level
-input wire clk;
-output reg clk1,clk2;
+    input wire clk;
+    output reg clk1,clk2;
 
-always @ ( posedge clk ) begin
-     clk1 = ~ clk1;
-     end
+    always @ ( posedge clk ) begin
+         clk1 = ~ clk1;
+    end
 
-always @ (posedge clk1) begin
-        clk2=~clk2;
-        end
+    always @ (posedge clk1) begin
+            clk2=~clk2;
+    end
 endmodule
