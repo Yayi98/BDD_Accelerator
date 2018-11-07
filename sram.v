587c07971a93bc42fd875a71f5b8721d89ad14d7
mahesh15230 <36959431+mahesh15230@users.noreply.github.com>
2018-11-07 15:30:12 +0530

    Update sram.v

--- sram.v
+++ sram.v
@@ -8,24 +8,19 @@ module sram #(parameter ADDR_WIDTH = 8, DATA_WIDTH = 34, DEPTH = 8) (
 
 	// Declare the RAM variable
 	reg [DATA_WIDTH-1:0] ram [0:DEPTH-1];
-    reg [DATA_WIDTH-1:0] q_a;
+    	reg [DATA_WIDTH-1:0] q_a;
 	// Port A
-	always @ (posedge clk)
-	begin
-		if (we_a)
-		begin
+	always @ (posedge clk) begin
+		if (we_a) begin
 			ram[addr_a] <= data_a;
 			q_a <= data_a;
-		end
-		else
-		begin
+		end else begin
 			q_a <= ram[addr_a];
 		end
 	end
 
 	// Port B
-	always @ (posedge clk)
-	begin
+	always @ (posedge clk) begin
 		// if (we_b)
 		// begin
 		// 	ram[addr_b] <= data_b;
