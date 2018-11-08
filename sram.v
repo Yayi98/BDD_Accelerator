0202dfe15ff2fad0066fa0609b32cdc8199d4aa4
Jatin Kumar Bollareddy <jatinkumarbollareddy98@gmail.com>
2018-11-08 23:37:02 +0530

    Add files via upload

--- sram.v
+++ sram.v
@@ -1,8 +1,8 @@
-module sram #(parameter ADDR_WIDTH = 8, DATA_WIDTH = 34, DEPTH = 8) (
+module sram #(parameter ADDR_WIDTH = 3, DATA_WIDTH = 34, DEPTH = 8) (
 
-	input [DATA_WIDTH-1:0] data_a, data_b,
+	input [DATA_WIDTH-1:0] data_a,
 	input [ADDR_WIDTH-1:0] addr_a, addr_b,
-	input we_a, clk,
+	input we_a,
 	output reg [DATA_WIDTH-1:0] q_b
 );
 
@@ -10,7 +10,7 @@ module sram #(parameter ADDR_WIDTH = 8, DATA_WIDTH = 34, DEPTH = 8) (
 	reg [DATA_WIDTH-1:0] ram [0:DEPTH-1];
 	reg [DATA_WIDTH-1:0] q_a;
 	// Port A
-	always @ (posedge clk) begin
+	always @ (addr_a) begin
 		if (we_a) begin
 			ram[addr_a] <= data_a;
 			q_a <= data_a;
@@ -20,7 +20,7 @@ module sram #(parameter ADDR_WIDTH = 8, DATA_WIDTH = 34, DEPTH = 8) (
 	end
 
 	// Port B
-	always @ (posedge clk) begin
+	always @ (addr_b) begin
 		// if (we_b)
 		// begin
 		// 	ram[addr_b] <= data_b;
